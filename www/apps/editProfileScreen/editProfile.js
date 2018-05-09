846a664baadaa4d2b5d6ad59c4eac1ea0ca25a7a
slnn3r <lustdxz@hotmail.com>
2018-05-09 12:03:43 +0800

    editProfile settled

--- www/apps/editProfileScreen/editProfile.js
+++ www/apps/editProfileScreen/editProfile.js
@@ -5,21 +5,131 @@ editProfileControl.controller('editProfileController', editProfileController);
 
 function editProfileController($scope, $http, $window) {
 
+  var loginData = JSON.parse(localStorage.getItem("loginData"));
+
+  $http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
+
+  $scope.isDisabled = true;
   $scope.socialInput = '';
+  $scope.emailInput = '';
+  $scope.mobileInput = '';
   $scope.oldPasswordInput = '';
   $scope.newPasswordInput = '';
   $scope.retypePasswordInput ='';
-  $scope.isDisabled = false;
+
+  $scope.getProfileInfo = function(){
+
+    $http({
+             method: 'POST',
+             data: {
+                 'userID' : loginData.UserID
+             },
+             url: 'https://flash-schedules.000webhostapp.com/getProfileInfo.php'
+          }).then(function (response){
+
+             $scope.socialInput = response.data[0].UserSocial;
+             $scope.emailInput = response.data[0].UserEmail;
+             $scope.mobileInput = response.data[0].UserMobile;
+
+             $scope.isDisabled = false;
+
+          },function (error){
+               alert("Please ensure You are connected to Internet.");
+               $scope.isDisabled = false;
+          });
+
+  }
+
 
   $scope.editInfoSubmit = function() {
 
     $scope.isDisabled = true;
 
+    $http({
+             method: 'POST',
+             data: {
+                 'userID' : loginData.UserID,
+                 'userSocial' : $scope.socialInput,
+                 'userEmail' : $scope.emailInput,
+                 'userMobile' : $scope.mobileInput
+
+             },
+             url: 'https://flash-schedules.000webhostapp.com/editProfileInfo.php'
+          }).then(function (response){
+
+             if(response.data[0]=="DONE"){
+
+               alert("Edit Profile Info Successful.");
+               $scope.getProfileInfo();
+               $scope.isDisabled = false;
+
+             }else if(response.data[0]=="EMAIL"){
+
+               alert("Email Have Been Used. Please Try Again.");
+               $scope.isDisabled = false;
+
+             }else if(response.data[0]=="MOBILE"){
+
+               alert("Mobile Number Have Been Used. Please Try Again.");
+               $scope.isDisabled = false;
+
+             }else{
+
+               alert("Edit Profile Info Failed. Please Try Again.");
+               $scope.isDisabled = false;
+
+             }
+
+          },function (error){
+               alert("Please ensure You are connected to Internet.");
+               $scope.isDisabled = false;
+          });
+
+
   }
 
-  $scope.changePasswordSubmit = function() {
+  $scope.editPasswordSubmit = function() {
 
     $scope.isDisabled = true;
+
+    $http({
+             method: 'POST',
+             data: {
+                 'userID' : loginData.UserID,
+                 'oldPassword' : $scope.oldPasswordInput,
+                 'newPassword' : $scope.newPasswordInput,
+
+             },
+             url: 'https://flash-schedules.000webhostapp.com/editProfilePassword.php'
+          }).then(function (response){
+
+             if(response.data[0]=="DONE"){
+
+               alert("Change Password Successful.");
+               $scope.oldPasswordInput = '';
+               $scope.newPasswordInput = '';
+               $scope.retypePasswordInput ='';
+               $scope.editProfilePassword.$setPristine();
+               $scope.isDisabled = false;
+
+
+             }else if(response.data[0]=="WRONG"){
+
+               alert("Old Password Incorrect. Please Try Again.");
+               $scope.isDisabled = false;
+
+             }else{
+
+               alert("Change Password Failed. Please Try Again.");
+               $scope.isDisabled = false;
+
+             }
+
+          },function (error){
+               alert("Please ensure You are connected to Internet.");
+               $scope.isDisabled = false;
+          });
+
 
   }
 
@@ -81,6 +191,13 @@ var app = {
     }
 
 };
+
+function displayUserInfo() {
+    var loginData = JSON.parse(localStorage.getItem("loginData"));
+
+    document.getElementById("userNameSpan").innerHTML = loginData.UserName;
+    document.getElementById("userTeamSpan").innerHTML = loginData.TeamName;
+}
 
 function exitApp(){
   navigator.app.exitApp();
